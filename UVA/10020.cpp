286f49f55eab834bcf64af040bed2f3742b7e104
rammendozaa <rammendozaa@yahoo.com>
2018-11-20 19:21:18 -0600

    AC

--- UVA/10020.cpp
+++ UVA/10020.cpp
@@ -18,63 +18,45 @@ int main(void)
         bool encontrado = false, neg = false;
         while(cin >> izq >> der && (izq != 0 || der != 0))
             izqDers.push_back(make_pair(izq,der));
-            /*
-            if(izq <= 0 && der >= M)
+        encontrado = false;
+        vector <pair<int, int> > pedazos;
+        sort(izqDers.begin(), izqDers.end());
+        int pivoteIzq = 0, agregarPos, limDer;
+        bool agregarValores = true;
+        int i = 0;
+        while(agregarValores)
+        {
+            limDer = pivoteIzq;
+            agregarValores = false;
+            while(i < izqDers.size() && izqDers[i].first <= pivoteIzq)
             {
-                auxI = izq;
-                auxD = der;
+                if(izqDers[i].second > limDer)
+                {
+                    agregarPos = i;
+                    limDer = izqDers[i].second;
+                    agregarValores = true;
+                }
+                i++;
+            }
+            pivoteIzq = limDer;
+            if(agregarValores)
+                pedazos.push_back(izqDers[agregarPos]);
+            if(limDer >= M)
+            {
                 encontrado = true;
-            }
-            if(izq <= 0)
-                neg = true;
-            */
-        /*
-        if(!neg)
-            cout << "0" << "\n";
-        else if(encontrado)
-            cout << "1\n" << auxI << " " << auxD << "\n";
-        */
-            encontrado = false;
-            vector <pair<int, int> > pedazos;
-            sort(izqDers.begin(), izqDers.end());
-            int pivoteIzq = 0, agregarPos, limDer;
-            bool agregarValores = true;
-            int i = 0;
-            while(agregarValores)
-            {
-                limDer = pivoteIzq;
                 agregarValores = false;
-                while(i < izqDers.size() && izqDers[i].first <= pivoteIzq)
-                {
-                    if(izqDers[i].second > limDer)
-                    {
-                        agregarPos = i;
-                        limDer = izqDers[i].second;
-                        agregarValores = true;
-                    }
-                    i++;
-                }
-                pivoteIzq = limDer;
-                if(agregarValores)
-                    pedazos.push_back(izqDers[agregarPos]);
-                if(limDer >= M)
-                {
-                    encontrado = true;
-                    agregarValores = false;
-                    break;
-                }
+                break;
             }
-            if(!encontrado)
-                cout << "0\n";
-            else
-            {
-                cout << pedazos.size() << "\n";
-                sort(pedazos.begin(), pedazos.end());
-                for(auto a : pedazos)
-                    cout << a.first << " " << a.second << "\n";
-            }
-
-
+        }
+        if(!encontrado)
+            cout << "0\n";
+        else
+        {
+            cout << pedazos.size() << "\n";
+            sort(pedazos.begin(), pedazos.end());
+            for(auto a : pedazos)
+                cout << a.first << " " << a.second << "\n";
+        }
         if(cases > 0)
             cout << "\n";
     }
