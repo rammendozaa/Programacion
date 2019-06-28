afaef70e7688397a452a4f8b0fe12a4ae5db05c2
Alvaro Mendoza Ramirez <rammendozaa@yahoo.com>
2019-06-27 20:11:01 -0500

    AC

--- UVA/893.cpp
+++ UVA/893.cpp
@@ -1,52 +1,4 @@
-/*
-vector <int> accDays(13, 0);
-unordered_map <int, int> monthDays;
-unordered_set <int> leapYears;
-
-bool isLeap(int year){
-	return (year % 4 == 0) ? (year % 100 == 0) ? (year % 400 == 0) ? true : false : true : false;
-}
-
-int remDaysYear(int day, int month, int year){
-	int remDay = 365;
-	if(isLeap(year))
-		remDay = 366;
-	remDay -= (accDays[month - 1] + day);
-}
-
-int remDaysMonth(int day, int month, int year){
-	int remDay = monthDays[month];
-	if(isLeap(year) && month == 2)
-		remDay++;
-	return remDay - day;
-}
-
-int main(void){
-	int n, day, month, year;
-	monthDays[1] = 31; 
-	monthDays[2] = 28; 
-	monthDays[3] = 31; 
-	monthDays[4] = 30; 
-	monthDays[5] = 31; 
-	monthDays[6] = 30; 
-	monthDays[7] = 31; 
-	monthDays[8] = 31; 
-	monthDays[9] = 30; 
-	monthDays[10] = 31; 
-	monthDays[11] = 30; 
-	monthDays[12] = 31; 
-	accDays[0] = 0;
-	for(int i = 1 ; i <= 12 ; i++)
-		accDays[i] = (monthDays[i] + accDays[i - 1]); 
-	while(cin >> n >> day >> month >> year && (n != 0 && day != 0 && month != 0 && year != 0)){
-
-
-
-	}
-	return 0;
-}
-*/
-
+/* AC */
 #include <iostream>
 #include <unordered_map>
 #include <vector> 
@@ -86,32 +38,22 @@ void findDateOtro(long long int day, long long int month, long long int year, lo
 	vector <long long int> monthAcc = daysPass;
 	if(isLeap(year))
 		dayMonths[2] = 29;
+	else
+		dayMonths[2] = 28; 
 	long long int daysLeftMonth = dayMonths[month] - day;
-	cout << dayMonths[month] << "  |  ";
+	//cout << day << " " << month << " " << year << " " << month << " 2 " << daysLeftMonth << " \t|\t";
 	if(daysLeftMonth > n) 	
 		cout << day + n << " " << month << " " << year << "\n";
 	else if(daysLeftMonth == n)
-		cout << 1 << " " << month + 1 << " " << year << "\n";
+		cout << dayMonths[month] << " " << month << " " << year << "\n";
 	else{
-		cout << day << " " << month << " " << year << " \t||\t" << daysLeftMonth << "\t|\t" << n << "\t|\t";
 		n -= daysLeftMonth;
 		month++;
-		while(n >= dayMonths[month]){
-			n -= dayMonths[month];
-			month++;
-		}
-		cout << n << " " << month << " " << year << "\n";
-		/*
-		n -= daysLeftMonth;
-		month++;
-		day = 1;
-		cout << day << " " << month << " " << year << " \t||\t" << daysLeftMonth << "\t|\t" << n << "\t|\t";
 		while(n > dayMonths[month]){
 			n -= dayMonths[month];
 			month++;
 		}
-		cout << ((n == 0) ? 1 : day + n) << " " << month << " " << year << "\n";
-		*/
+		cout << n << " " << month << " " << year << "\n";
 	} 
 }	
 
@@ -129,6 +71,8 @@ int main(void){
 	dayMonths[11] = 30;
 	dayMonths[12] = 31;
 	daysPass[0] = 0;
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
 	for(long long int i = 1 ; i <= 12 ; i++)
 		daysPass[i] = (daysPass[i - 1] + dayMonths[i]);
 	long long int n, day, month, year, remDays;
